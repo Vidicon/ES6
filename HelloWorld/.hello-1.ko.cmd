9539fa5468cc27a804c421fc820fff5083d1a5a7
Minh-Triet Diep <minhtrietdiep@gmail.com>
2017-03-10 09:29:24 +0100

    Add Hello World

---
+++ HelloWorld/.hello-1.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/student/ES6/HelloWorld/hello-1.ko := arm-linux-ld -EL -r  -T /home/student/felabs/sysdev/tinysystem/linux-2.6.34/scripts/module-common.lds --build-id -o /home/student/ES6/HelloWorld/hello-1.ko /home/student/ES6/HelloWorld/hello-1.o /home/student/ES6/HelloWorld/hello-1.mod.o
---
+++ HelloWorld/.hello-2.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/student/ES6/HelloWorld/hello-2.ko := arm-linux-ld -EL -r  -T /home/student/felabs/sysdev/tinysystem/linux-2.6.34/scripts/module-common.lds --build-id -o /home/student/ES6/HelloWorld/hello-2.ko /home/student/ES6/HelloWorld/hello-2.o /home/student/ES6/HelloWorld/hello-2.mod.o
