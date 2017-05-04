e774507d986712931fd4edc1057167abf8093cee
Lars Jaeqx <lars.jaeqx@gmail.com>
2017-05-04 16:43:16 +0200

    pwm done

--- pwm/pwm.c
+++ pwm/pwm.c
@@ -1,221 +1,228 @@
-#include <linux/kernel.h>	/* We're doing kernel work */
-#include <linux/module.h>	/* Specifically, a module */
-#include <linux/kobject.h>   /* Necessary because we use sysfs */
-#include <linux/device.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/fs.h>
 #include <mach/hardware.h>
+#include <asm/uaccess.h>
 
-#define sysfs_dir  "es6"
-#define sysfs_file "pwm"
-#define SYSFS_FILE_MACRO pwm
+#define REG_PWM1			0x4005C000
+#define REG_PWM2			0x4005C004
+#define REG_PWMCLOCK		0x400040B8  //= 0x115
+#define REG_LCDCONFIG		0x40004054  //= 0
 
-#define PWM1		0x4005C000
-#define PWM2		0x4005C004
-#define PWMCLOCK	0x400040B8	//= 0x115
-#define LCDCONFIG	0x40004054	//= 0
-#define PWMEnable 	0x80000000
-#define PWMBitsToShiftForFreq 8 // lekker korte naam toch ;p
+#define MASK_PWM_ENABLE		0x80000000
+#define MASK_PWM_DUTY		0x000000FF
+#define MASK_PWM_FREQUENCY	0x0000FF00
 
-#define DEV_NAME "pwm_device"
-#define DEV_MAJOR	99
+#define PWM_SHIFT_FREQ		8 // lekker korte naam toch ;p
+#define PWM_SHIFT_ENABLE	31
 
-static ssize_t
-sysfs_pwm(struct device *dev,
-			struct device_attribute *attr,
-			const char *buffer,
-			size_t count)
-{	
-	char command = 'x';
-	int pwmNumber = -1;
-	int dutyCycle = -1;
-	int frequency = -1;
-	sscanf(buffer, "%c %d %d %d", &command, &pwmNumber, &frequency, &dutyCycle);
+#define VAL_PWMCLOCK		0x115
+#define VAL_LCDCONFIG		0
 
-	if (command == 'r') {
-		if(pwmNumber == 1) {
-			printk(KERN_INFO "pwm1: %x\n", *(unsigned int*)(io_p2v(PWM1)));
-		}
-		else if(pwmNumber == 2) {
-			printk(KERN_INFO "pwm2: %x\n", *(unsigned int*)(io_p2v(PWM2)));
-		}
-		else {
-			printk(KERN_INFO "doe eens niet...\n");
-		}
-	} else if (command == 'w') {
-		if(pwmNumber == 1) {
-			if(frequency >= 0 && frequency < 256 && dutyCycle >= 0 && dutyCycle < 256) {
-				*(unsigned int*)(io_p2v(PWM1)) = PWMEnable;
-				*(unsigned int*)(io_p2v(PWM1)) |= frequency << PWMBitsToShiftForFreq;
-				*(unsigned int*)(io_p2v(PWM1)) |= dutyCycle;
-				printk(KERN_INFO "pwm1 geschreven\n");
-			} else {
-				printk(KERN_INFO "nope...\n");
-			}
-			
-		}
-		else if(pwmNumber == 2) {
-			if(frequency >= 0 && frequency < 256 && dutyCycle >= 0 && dutyCycle < 256) {
-				*(unsigned int*)(io_p2v(PWM2)) = PWMEnable;
-				*(unsigned int*)(io_p2v(PWM2)) |= frequency << PWMBitsToShiftForFreq;
-				*(unsigned int*)(io_p2v(PWM2)) |= dutyCycle;
-				printk(KERN_INFO "pwm2 geschreven\n");
-			} else {
-				printk(KERN_INFO "nope...\n");
-			}
-		} else {			
-			printk(KERN_INFO "doe eens niet...\n");
-		}
-	} else {
-		printk(KERN_INFO "doe eens niet...\n");
+#define PWMCLOCKFREQ		32000
+
+#define DEVICE_NAME			"chardev"
+#define DEVICE_MAJOR		103
+
+#define BUF_LEN				80
+
+static int deviceIsOpen = 0;
+static char Message[BUF_LEN];
+static char *Message_Ptr;
+
+static int device_open(struct inode *inode, struct file *file) {
+	int minor = MINOR(inode->i_rdev);
+
+	Message_Ptr = Message;
+	
+	file->private_data = (void*)minor;
+
+	if (deviceIsOpen) return -EBUSY;
+	deviceIsOpen++;
+
+	printk(KERN_INFO "Major: %d, minor: %d", MAJOR(inode->i_rdev), minor);
+
+	return 0;
+}
+
+static int device_release(struct inode *inode, struct file *file) {
+	deviceIsOpen--;
+	return 0;
+}
+
+static int map_freq(int pwm_reloadvalue) {
+	return (PWMCLOCKFREQ / pwm_reloadvalue) / 256;
+}
+
+static int map_duty(int pwm_dutyvalue) {
+	return (pwm_dutyvalue * 100) / 256;
+}
+
+static ssize_t device_read(struct file *filp, char *buffer, size_t length, loff_t *offset) {
+	int bytes_read = 0;
+	int minor = (int)filp->private_data;
+
+	int PWM1Value = *(unsigned int*)(io_p2v(REG_PWM1));
+	int PWM2Value = *(unsigned int*)(io_p2v(REG_PWM2));
+
+	if (*Message_Ptr == 0) {
+		return 0;
 	}
+    
+	switch(minor) {
+		case 0: {
+			bool enabled = (PWM1Value & MASK_PWM_ENABLE) != 0;			
+			sprintf(Message_Ptr, "PWM1 enabled: %s", enabled ? "Yes" : "No");
+			break;
+		}
+		case 1: {
+			int reloadValue = (PWM1Value & MASK_PWM_FREQUENCY) >> PWM_SHIFT_FREQ;
+			if(reloadValue == 0) {
+				sprintf(Message_Ptr, "nope..."); // iets met delen door 0
+				break;
+			}
+			sprintf(Message_Ptr, "PWM1 raw: %d, freq: %dHz", reloadValue, map_freq(reloadValue));
+			break;
+		}
+		case 2: {
+			int dutyValue = (PWM1Value & MASK_PWM_DUTY);
+			if(dutyValue == 0) {
+				sprintf(Message_Ptr, "nope..."); // iets met delen door 0
+				break;
+			}
+			sprintf(Message_Ptr, "PWM1 raw: %d, duty: %d%%", dutyValue, map_duty(dutyValue));
+			break;
+		}
+		case 3: {
+			bool enabled = (PWM2Value & MASK_PWM_ENABLE) != 0;
+			sprintf(Message_Ptr, "PWM2 enabled: %s", enabled ? "Yes" : "No");
+			break;
+		}
+		case 4: {
+			int reloadValue = (PWM2Value & MASK_PWM_FREQUENCY) >> PWM_SHIFT_FREQ;
+			if(reloadValue == 0) {
+				sprintf(Message_Ptr, "nope..."); // iets met delen door 0
+				break;
+			}
+			sprintf(Message_Ptr, "PWM2 raw: %d, freq: %dHz", reloadValue, map_freq(reloadValue));
+			break;
+		}
+		case 5: {
+			int dutyValue = (PWM2Value & MASK_PWM_DUTY);
+			if(dutyValue == 0) {
+				sprintf(Message_Ptr, "nope..."); // iets met delen door 0
+				break;
+			}
+			sprintf(Message_Ptr, "PWM2 raw: %d, duty: %d%%", dutyValue, map_duty(dutyValue));
+			break;
+		}
+		default:
+			sprintf(Message_Ptr, "nope break...");
+			break;
+	}
+
+	while (length && *Message_Ptr) {
+	    put_user(*(Message_Ptr++), buffer++);
+	    length--;
+	    bytes_read++;
+  	}
+
+	return bytes_read;
+}
+
+static ssize_t device_write(struct file *filp, const char *buff, size_t len, loff_t *off) {
+	int minor = (int)filp->private_data;
+	int PWM1Value = *(unsigned int*)(io_p2v(REG_PWM1));
+	int PWM2Value = *(unsigned int*)(io_p2v(REG_PWM2));
+	int valueToWrite = -1;
+	int i;
+
+	for (i = 0; i < len && i < BUF_LEN; i++) {
+    	get_user(Message[i], buff + i);
+	}
+
+  	Message_Ptr = Message;
+	
+	sscanf(Message_Ptr, "%d", &valueToWrite);
+
+	if(valueToWrite < 0 && valueToWrite > 255) {
+		printk(KERN_INFO "nope...");
+		return len;
+	}
+
+	switch(minor) {
+		case 0: {
+			*(unsigned int*)(io_p2v(REG_PWM1)) ^= (-valueToWrite ^ PWM1Value) & (1 << PWM_SHIFT_ENABLE);
+			printk(KERN_INFO "PWM1 enable written");
+			break;
+		}
+		case 1: {
+			int mask = (valueToWrite << PWM_SHIFT_FREQ) & MASK_PWM_FREQUENCY;
+			PWM1Value = PWM1Value & ~MASK_PWM_FREQUENCY;
+			*(unsigned int*)(io_p2v(REG_PWM1)) = PWM1Value | mask;
+			printk(KERN_INFO "PWM1 freq written");
+			break;
+		}
+		case 2: {
+			int mask = valueToWrite & MASK_PWM_DUTY;
+			PWM1Value = PWM1Value & ~MASK_PWM_DUTY;
+			*(unsigned int*)(io_p2v(REG_PWM1)) = PWM1Value | mask;
+			printk(KERN_INFO "PWM1 duty written");
+			break;
+		}
+		case 3: {
+			*(unsigned int*)(io_p2v(REG_PWM2)) ^= (-valueToWrite ^ PWM2Value) & (1 << PWM_SHIFT_ENABLE);
+			printk(KERN_INFO "PWM2 enable written");
+			break;
+		}
+		case 4: {
+			int mask = (valueToWrite << PWM_SHIFT_FREQ) & MASK_PWM_FREQUENCY;
+			PWM2Value = PWM2Value & ~MASK_PWM_FREQUENCY;
+			*(unsigned int*)(io_p2v(REG_PWM2)) = PWM2Value | mask;
+			printk(KERN_INFO "PWM2 freq written");
+			break;
+		}
+		case 5: {
+			int mask = valueToWrite & MASK_PWM_DUTY;
+			PWM2Value = PWM2Value & ~MASK_PWM_DUTY;
+			*(unsigned int*)(io_p2v(REG_PWM2)) = PWM2Value | mask;
+			printk(KERN_INFO "PWM2 duty written");
+			break;
+		}
+		default:
+			printk(KERN_INFO "nope break...");
+			break;
+	}
+	return i;
+}
+
+static struct file_operations fops = {
+	.read = device_read,
+	.write = device_write,
+	.open = device_open,
+	.release = device_release
+};
+
+int init_module(void) {
+	int major = register_chrdev(DEVICE_MAJOR, DEVICE_NAME, &fops);
+
+	if (major < 0) {
+		printk ("Registering the character device failed");
+		return major;
+	}
+	printk("Registering the character device succesfull");
+
+	*(unsigned int*)(io_p2v(REG_PWMCLOCK)) = VAL_PWMCLOCK;
+	*(unsigned int*)(io_p2v(REG_LCDCONFIG)) = VAL_LCDCONFIG;
 
 	return 0;
 }
 
 
-/* 
- * This line is now changed: in the previous example, the last parameter to DEVICE_ATTR
- * was NULL, now we add a store function as well. We must also add writing rights to the file:
- */
-static DEVICE_ATTR(SYSFS_FILE_MACRO, S_IWUGO | S_IRUGO, NULL, sysfs_pwm);
-
-/*
- * This is identical to previous example.
- */
-static struct attribute *attrs[] = {
-	&dev_attr_SYSFS_FILE_MACRO.attr,
-	NULL   /* need to NULL terminate the list of attributes */
-};
-static struct attribute_group attr_group = {
-	.attrs = attrs,
-};
-static struct kobject *pwm_obj = NULL;
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-#include <linux/fs.h>
-#include <linux/io.h>
-#include <asm/uaccess.h>  /* for put_user */
-#include <asm/errno.h>
-
-/* Global variables */
-#define BUF_LEN 80            /* Max length of the message from the device */
-#define SUCCESS 0
-#define DEVICE_NAME "pwm" /* Dev name as it appears in /proc/devices   */
-static int Device_Open = 0;  /* Is device open?  Used to prevent multiple access to the device */
-static char msg[BUF_LEN];    /* The msg the device will give when asked    */
-static char *msg_Ptr;
-
-/* Called when a process tries to open the device file, like
- * "cat /dev/mycharfile"
- */
-static int device_open(struct inode *inode, struct file *file)
-{
-   if (Device_Open) return -EBUSY;
-
-   Device_Open++;
-   msg_Ptr = msg;
-
-   return SUCCESS;
+void cleanup_module(void) {
+	unregister_chrdev(DEVICE_MAJOR, DEVICE_NAME);
 }
 
-
-/* Called when a process closes the device file */
-static int device_release(struct inode *inode, struct file *file)
-{
-   Device_Open --;     /* We're now ready for our next caller */
-
-   return 0;
-}
-
-/* Called when a process, which already opened the dev file, attempts to
-   read from it.
-*/
-static ssize_t device_read(struct file *filp,
-   char *buffer,    /* The buffer to fill with data */
-   size_t length,   /* The length of the buffer     */
-   loff_t *offset)  /* Our offset in the file       */
-{
-   /* Number of bytes actually written to the buffer */
-   int bytes_read = 0;
-
-   /* If we're at the end of the message, return 0 signifying end of file */
-   if (*msg_Ptr == 0) return 0;
-
-   /* Actually put the data into the buffer */
-   while (length && *msg_Ptr)  {
-
-        /* The buffer is in the user data segment, not the kernel segment;
-         * assignment won't work.  We have to use put_user which copies data from
-         * the kernel data segment to the user data segment. */
-         put_user(*(msg_Ptr++), buffer++);
-
-         length--;
-         bytes_read++;
-   }
-
-   /* Most read functions return the number of bytes put into the buffer */
-   return bytes_read;
-}
-
-
-/*  Called when a process writes to dev file: echo "hi" > /dev/hello */
-static ssize_t device_write(struct file *filp,
-   const char *buff,
-   size_t len,
-   loff_t *off)
-{
-   printk ("<1>Sorry, this operation isn't supported.\n");
-   return -EINVAL;
-}
-
-static struct file_operations Fops = {
-    .read = device_read,
-    .write = device_write,
-    .open = device_open,
-    .release = device_release,
-};
-
-
-int __init sysfs_init(void)
-{
-	int result = 0;
-
-	pwm_obj = kobject_create_and_add(sysfs_dir, kernel_kobj);
-	if (pwm_obj == NULL)
-	{
-		printk (KERN_INFO "%s module failed to load: kobject_create_and_add failed\n", sysfs_file);
-		return -ENOMEM;
-	}
-
-	result = sysfs_create_group(pwm_obj, &attr_group);
-	if (result != 0)
-	{
-		/* creating files failed, thus we must remove the created directory! */
-		printk (KERN_INFO "%s module failed to load: sysfs_create_group failed with result %d\n", sysfs_file, result);
-		kobject_put(pwm_obj);
-		return -ENOMEM;
-	}
-
-	printk(KERN_INFO "/sys/kernel/%s/%s created\n", sysfs_dir, sysfs_file);
-
-   	register_chrdev(DEV_MAJOR, DEV_NAME, &Fops);
-
-	*(unsigned int*)(io_p2v(PWMCLOCK)) = 0x115;
-	*(unsigned int*)(io_p2v(LCDCONFIG)) = 0;
-
-
-	return result;
-}
-
-void __exit sysfs_exit(void)
-{
-   	unregister_chrdev(DEV_MAJOR, DEV_NAME);
-
-	kobject_put(pwm_obj);
-	printk (KERN_INFO "/sys/kernel/%s/%s removed\n", sysfs_dir, sysfs_file);
-}
-
-module_init(sysfs_init);
-module_exit(sysfs_exit);
 MODULE_LICENSE("Dual BSD/GPL");
 MODULE_AUTHOR("LJ&MT");
 MODULE_DESCRIPTION("pwm");
